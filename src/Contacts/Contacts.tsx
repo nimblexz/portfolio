431c1f8399e1c91c49fa588b00b42b1167b4d146
nimblexz <109966757+nimblexz@users.noreply.github.com>
2023-02-22 03:25:10 +0400

    tg

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -12,8 +12,9 @@ type FormData = {
 };
 
 export function Contacts() {
-    const {register, handleSubmit, reset, formState: {errors}} = useForm<FormData>(
+    const {register, handleSubmit, reset, formState: {errors,isValid}} = useForm<FormData>(
         {
+            mode:"onBlur",
             defaultValues: {
                 name: '',
                 eMail: '',
@@ -22,7 +23,7 @@ export function Contacts() {
         }
     );
     const onSubmit = (data: FormData) => {
-        axios.post('https://gmail-6gf19icax-hhfvbkm1973-gmailcom.vercel.app/send',data).then(()=>console.log('213')).catch((err)=>(console.log(err)))
+        axios.post('https://gmail-6gf19icax-hhfvbkm1973-gmailcom.vercel.app/send', data).then(() => console.log('213')).catch((err) => (console.log(err)))
         reset()
     }
 
@@ -33,10 +34,26 @@ export function Contacts() {
                 <Title title={'Contacts'}/>
                 <Slide className={s.slide} direction={"right"}>
                     <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-                        <input {...register("name")} className={s.input} placeholder={'name'}/>
-                        <input {...register("eMail")} placeholder={'e-mail'} className={s.input}/>
-                        <textarea {...register("message")} placeholder={'message'} className={s.textarea}/>
-                        <Button title={'Send'} backgroundColor={'#0b0b0b'} isbutton={true}/>
+                        <input {...register("name", {required: 'Name required',minLength:{value:3,message:'Minimum of 3 symbols'}})} className={s.input}
+                               placeholder={'name'} />
+                        <div style={{
+                            height: 30,
+                            color: 'red',
+                        }}>{errors?.name && <>{errors?.name?.message || 'error'}</>}</div>
+                        <input {...register("eMail", {pattern:/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,required: 'E-mail required',})} placeholder={'e-mail'}
+                               className={s.input} />
+                        <div style={{
+                            height: 30,
+                            color: 'red'
+                        }}>{errors?.eMail && <>{errors?.eMail?.message || 'Incorrect E-mail'}</>}</div>
+                        <textarea {...register("message",{required: 'Message required',minLength:{value:10,message:'Minimum of 10 symbols'}})} placeholder={'message'}
+                                  className={s.textarea}/>
+                        <div style={{
+                            height: 30,
+                            color: 'red'
+                        }}>{errors?.message && <>{errors?.message?.message || 'error'}</>}</div>
+
+                        <Button title={'Send'} backgroundColor={'#0b0b0b'} isbutton={true} disabled={!isValid}/>
                     </form>
                 </Slide>
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,11 +6,12 @@ type ButtonProps = {
     title: string
     backgroundColor: string
     isbutton:boolean
+    disabled?:any
 }
 
 export function Button(props: ButtonProps) {
     return <div className={s.Button}>
-        {props.isbutton ? <button type="submit" style={{backgroundColor: props.backgroundColor}}
+        {props.isbutton ? <button  disabled={props.disabled} type="submit" style={{backgroundColor: props.backgroundColor,opacity:props.disabled?0.5:1}}
                 className={s.button}>{props.title}</button>: <Link className={s.button} href='' to='contacts' activeClass={s.active} spy={true} smooth={true} offset={1} duration={500}>Hire me</Link>}
     </div>
 }
