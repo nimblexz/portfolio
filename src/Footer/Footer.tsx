2348a3459db2e0faaabfa625a2a9a64995cfa498
nimblexz <109966757+nimblexz@users.noreply.github.com>
2023-03-14 17:46:08 +0400

    tg

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -11,22 +11,22 @@ export function Footer() {
                 <Title title={'Emil Khayrulin'}/>
                 <div className={s.footerDiv}>
                     <div className={s.div}>
-                        <a href='https://t.me/nimblexz' target={'_blank'}>
+                        <a className={s.a} href='https://t.me/nimblexz' target={'_blank'}>
                             <img src={telegramicon} className={s.icon} alt='telegram'/>
                         </a>
                     </div>
                     <div className={s.div}>
-                        <a href='https://www.linkedin.com/in/nimblexz/' target={'_blank'}>
+                        <a className={s.a} href='https://www.linkedin.com/in/nimblexz/' target={'_blank'}>
                             <img src={linkedinicon} className={s.icon} alt='linkedin'/>
                         </a>
                     </div>
                     <div className={s.div}>
-                        <a href='https://github.com/nimblexz' target={'_blank'}>
+                        <a className={s.a} href='https://github.com/nimblexz' target={'_blank'}>
                             <img className={s.icon} src={githubicon} alt='github'/>
                         </a>
                     </div>
                     <div className={s.div}>
-                        <a href='https://www.codewars.com/users/nimblexz' target={'_blank'}>
+                        <a className={s.a} href='https://www.codewars.com/users/nimblexz' target={'_blank'}>
                             <img src={codewarsicon} className={s.icon} alt='codewars'/>
                         </a>
                     </div>
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -11,7 +11,7 @@ type ButtonProps = {
 
 export function Button(props: ButtonProps) {
     return <div className={s.Button}>
-        {props.isbutton ? <button  disabled={props.disabled} type="submit" style={{backgroundColor: props.backgroundColor,opacity:props.disabled?0.5:1}}
+        {props.isbutton ? <button  disabled={props.disabled} type="submit" style={{backgroundColor: props.backgroundColor}}
                 className={s.button}>{props.title}</button>: <Link className={s.button} href='' to='contacts' activeClass={s.active} spy={true} smooth={true} offset={1} duration={500}>Hire me</Link>}
     </div>
 }
