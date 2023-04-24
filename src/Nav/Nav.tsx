155280d2cfa4904bade04bfc8219852fc43e1ca9
nimblexz <109966757+nimblexz@users.noreply.github.com>
2023-04-24 21:42:44 +0400

    f

--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -5,7 +5,7 @@ import {Link} from "react-scroll";
 export function Nav() {
     return (
         <div className={s.nav}>
-            <a  href={'https://drive.google.com/file/d/1xTzwa20mjTpJBK3dulozuvSlvfHo95J3/view?usp=share_link'} target={'_blank'}>My CV</a>
+            <a  href={'https://drive.google.com/file/d/1WAnOFOZBqYd-YQzQWcCrMIREjsrjEABS/view?usp=share_link'} target={'_blank'}>My CV</a>
             <Link href='' to='home' activeClass={s.active} spy={true} smooth={true} offset={1} duration={500}>Home</Link>
             <Link href='' to='skills' activeClass={s.active} spy={true} smooth={true} offset={1} duration={500}>Skills</Link>
             <Link href='' to='projects' activeClass={s.active} spy={true} smooth={true} offset={1} duration={500}>Projects</Link>
